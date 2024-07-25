de698a3d0fed2a12072926206666df58620be4a0
betelhem demsis <bettydemsis@gmail.com>
2024-07-25 20:31:48 +0300

    modified:task_2.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,14 +1,14 @@
-CREATE DATABASE IF NOT EXISTS alx_book_store;
+CREATE DATABASE alx_book_store;
 USE alx_book_store;
 
-CREATE TABLE IF NOT EXISTS Authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Authors (
+    author_id INT  PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
 
-CREATE TABLE IF NOT EXISTS Books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
     price DOUBLE NOT NULL,
@@ -16,23 +16,23 @@ CREATE TABLE IF NOT EXISTS Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE IF NOT EXISTS Customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
     address TEXT
 );
 
 
-CREATE TABLE IF NOT EXISTS Orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE IF NOT EXISTS Order_Details (
-    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE NOT NULL,
